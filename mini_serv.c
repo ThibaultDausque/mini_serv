4a57ecb7821c37df69e5f6d3adfa45a923e0e553
Thibault Dausque <tdausque@1E7.42angouleme.fr>
2025-11-23 12:46:44 +0100

    finished bro

--- mini_serv.c
+++ mini_serv.c
@@ -67,7 +67,7 @@ int accept_cli(int sockfd, t_cli *cli)
     else
     {
         cli->id = i;
-        printf("client %d connected.\n", i++);
+        printf("client %d just arrived.\n", i++);
         FD_SET(cli->fd, &master);
     }
     return cli->fd;
@@ -78,46 +78,61 @@ int start_server(int sockfd)
     int     bytes;
     t_cli   cli[1024];
     int     i = 0;
+    int     j = 0;
     int     retval;
 
     while (1)
     {
 		FD_SET(sockfd, &master);
-		FD_SET(STDIN_FILENO, &master);
-		FD_SET(STDOUT_FILENO, &master);
-		FD_SET(STDERR_FILENO, &master);
-		wfds = master;
-		printf("sockfd: %d \n in: %d\n out: %d\n err: %d\n", sockfd, STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO);
-		retval = select(1024, &wfds, NULL, NULL, NULL);
+		rfds = master;
+		retval = select(1024, &rfds, NULL, NULL, NULL);
 		if (!retval)
 		{
 			printf("select() error\n");
 			exit(0);
 		}
         i = 0;
-        while (i < 1024)
+        if (FD_ISSET(sockfd, &rfds))
         {
-			if (!FD_ISSET(i, &master))
-				break ;
-			if (i == sockfd)
-			{
-				accept_cli(sockfd, &cli[i]);
-				printf("fd: %d\n", i);
-			}
-			else
-			{
-				bytes = recv(i, cli[i].buff, sizeof(cli[i].buff), 0);
-				if (bytes <= 0)
-				{
-					printf("client %d disconnected.", i);
-					exit(0);
-				}
-				else
-				{
-					printf("client %d: %s\n", i, cli[i].buff);
-				}
-			}
-			i++;
+            if (accept_cli(sockfd, &cli[j]))
+            {
+                int     k = 0;
+                while (k != j)
+                {
+                    char    buff[1024];
+                    sprintf(buff, "client %d just arrived.\n", cli[j].id);
+                    send(cli[k].fd, buff, sizeof(buff), 0);
+                    k++;
+                }
+            }
+            j++;
+        }
+        while (i <= j)
+        {
+            if (FD_ISSET(cli[i].fd, &rfds))
+            {
+                char    buff[1024];
+                bytes = recv(cli[i].fd, buff, sizeof(buff), 0);
+                int     k = 0;
+                while (k != j)
+                {
+                    char    mess[1024];
+                    if (k != i)
+                    {
+                        sprintf(mess, "client %d: %s", cli[i].id, buff);
+                        send(cli[k].fd, mess, sizeof(mess), 0);
+                    }
+                    k++;
+                }
+                if (bytes <= 0)
+                {
+                    printf("client %d disconnected.", cli[i].id);
+                    close(cli[i].fd);
+                }
+                else
+                    printf("client %d: %s\n", i, buff);
+            }
+            i++;
         }
     }
     return 1;

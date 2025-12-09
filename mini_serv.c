9f70947fde3b8a096e0864d526c7e300022cbdb0
Thibault Dausque <tdausque@1E6.42angouleme.fr>
2025-12-09 16:58:49 +0100

    again

--- mini_serv.c
+++ mini_serv.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
@@ -5,10 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/select.h>
-# include <stdlib.h>
-# include <stdio.h>
-
-# define ERROR "Fatal Error"
+#include <stdio.h>
 
 typedef struct s_cli {
     int     fd;
@@ -16,103 +14,88 @@ typedef struct s_cli {
 }   t_cli;
 
 typedef struct s_serv {
+    int     sockfd;
+    t_cli   cli[1024];
     fd_set  master;
     fd_set  rfds;
-    fd_set  wfds;
-    int     nfds;
-    int     sockfd;
 }   t_serv;
 
-int init_serv(t_serv *serv, int port)
+int max = 0;
+int init_serv(int port, t_serv *serv)
 {
-    struct sockaddr_in  servaddr;
+  int sockfd;
+  struct sockaddr_in servaddr;
 
-    serv->sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serv->sockfd == -1)
-    {
-        printf("tutu");
-        write(0, ERROR, strlen(ERROR));
-        exit(0);
-    }
-    if (serv->sockfd > serv->nfds)
-        serv->nfds = serv->sockfd;
-    FD_SET(serv->sockfd, &serv->master);
-    FD_SET(serv->sockfd, &serv->rfds);
-    bzero(&servaddr, sizeof(servaddr));
+  // socket create and verification
+  serv->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (serv->sockfd == -1)
+  {
+    printf("socket creation failed...\n");
+    exit(0);
+  }
+  else
+    printf("Socket successfully created..\n");
+  bzero(&servaddr, sizeof(servaddr));
 
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(2130706433);
-    servaddr.sin_port = htons(port);
+  // assign IP, PORT
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
+  servaddr.sin_port = htons(port);
 
-    // Binding newly created socket to given IP and verification
-    if ((bind(serv->sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-    {
-        printf("toto");
-        write(0, ERROR, strlen(ERROR));
-        exit(0);
-    }
-    if (listen(serv->sockfd, 1024) != 0)
-    {
-        write(0, ERROR, strlen(ERROR));
-        exit(0);
-    }
-    return 1;
+  // Binding newly created socket to given IP and verification
+  if ((bind(serv->sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+  {
+    printf("socket bind failed...\n");
+    exit(0);
+  }
+  FD_SET(serv->sockfd, &serv->master);
+  max++;
+  return serv->sockfd;
 }
 
-int accept_cli(t_serv *serv, t_cli *cli)
+int accept_cli(t_cli *cli, int sockfd, t_serv *serv)
 {
+    socklen_t     len;
     struct sockaddr_in  client;
-    socklen_t   len = sizeof(client);
-    int         clifd;
 
-    clifd = accept(serv->sockfd, (struct sockaddr *)&client, &len);
-    cli->fd = clifd;
-    if (clifd < 0)
+    len = sizeof(client);
+    cli->fd = accept(sockfd, (struct sockaddr *)&client, &len);
+    if (cli->fd < 0)
     {
-        write(0, ERROR, strlen(ERROR));
-        exit(0);
+        printf("server accept failed...\n");
+        exit(1);
     }
     else
     {
-        if (clifd > serv->nfds)
-            serv->nfds = cli->fd;
-        cli->id = clifd - 4;
-        char    tab[1024];
-        sprintf(tab, "server: client %d arrived.\n", cli->fd);
-        // int     i = 0;
-        FD_SET(clifd, &serv->master);
-        FD_SET(clifd, &serv->rfds);
-        // while (i < cli->id)
-        // {
-        //     send(i + 4, &tab, sizeof(tab), 0);
-        //     i++;
-        // }
+        printf("server accept client...\n");
+        max++;
+        FD_SET(cli->fd, &serv->master);
     }
-    return clifd;
+    return cli->fd;
 }
 
 int main(int ac, char **av)
 {
+    if (ac != 2)
+    {
+        printf("too much args...\n");
+        exit(1);
+    }
     t_serv  serv;
     t_cli   cli[1024];
 
-    if (ac != 2)
-        exit(0);
+    int     i = 0;
     int     port = atoi(av[1]);
-    int     j = 0;
-
-    serv.nfds = 0;
-    init_serv(&serv, port);
+    int     sockfd = init_serv(port, &serv);
     while (1)
     {
         serv.rfds = serv.master;
-        select(serv.nfds, &serv.rfds, 0, 0, 0);
-        printf("serv socket: %d\n", serv.sockfd);
-        printf("nfds: %d\n", serv.nfds);
+        int nfd = select(max + 1, &serv.rfds, 0, 0, 0);
         if (FD_ISSET(serv.sockfd, &serv.rfds))
-            accept_cli(&serv, &cli[j++]);
-        printf("a client joined !\n");
+        {
+            accept_cli(&cli[i], sockfd, &serv);
+            cli[i].id = cli[i].fd - 4;
+            i++;
+        }
     }
-    return 0;
 }

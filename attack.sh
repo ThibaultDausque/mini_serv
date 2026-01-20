82efe34d26d2605d4b90a4fd24c12c41a1ab9bd9
Thibault Dausque <tdausque@1D6.42angouleme.fr>
2026-01-20 17:36:40 +0100

    perfect mini_serv

--- attack.sh
+++ attack.sh
@@ -9,30 +9,49 @@ NC='\033[0m'
 PORT=9999
 OBJ=./serv
 
+kill $(lsof -ti:$PORT) > /dev/null 2>&1
+
 # fail=0, success=1
 test=0
 compiled=0
 
+FLAG="-Wall -Wextra -Werror -g"
+
 echo "Test starting..."
-if cc -Wall -Wextra -Werror mini_serv.c -o ${OBJ}; then
+if cc ${FLAG} mini_serv.c -o ${OBJ}; then
     echo "${GREEN}project compiled"
-    while true; do
-        ./serv ${PORT}
-        if [ $? -eq 0 ]; then
-            echo "server runnning on port ${PORT}..."
-        else
-            echo "error"
-        fi
-    done
 else
     echo "compilation failed"
+    exit 1
 fi
 
-# # for ((i=0; i<100: i++)); do
-# #     printf "hello world"
-# # done
-# printf "Hello world!" | nc localhost ${PORT}
+touch server.log
+${OBJ} ${PORT} > server.log 2>&1 &
+PID=$!
+if [ $? -eq 0 ]; then
+    echo "server runnning on port ${PORT}..."
+else
+    echo "${RED}Fatal error${NC}"
+fi
 
-# if [ ${test} -eq 0 ];then
-#     echo "${GREEN}test succeed !${NC}"
-# fi
+nc localhost ${PORT} &
+NCPID=$!
+if [ $? -eq 0 ]; then
+    echo "${GREEN}test 1: OK${NC}"
+else
+    echo "${RED}test 1: KO${NC}"
+fi
+
+cat server.log
+
+if ps -p $PID > /dev/null; then
+    kill $PID
+fi
+
+if ps -p $NCPID > /dev/null; then
+    kill $NCPID
+fi
+
+if [ $? -eq 0 ]; then
+    echo "done"
+fi
--- mini_serv.c
+++ mini_serv.c
@@ -4,80 +4,22 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <sys/select.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct  cli_s
+{
+  int   id;
+  int   fd;
+} cli_t;
 
 typedef struct serv_s
 {
-    int     sockfd;
-    fd_set  master;
-    fd_set  rfds;
-}   serv_t;
-
-typedef struct cli_s
-{
-    int     fd;
-    int     id;
-}   cli_t;
-
-void    ft_message(int fd, char *buff)
-{
-    int     i = 0;
-    while (buff[i])
-        write(fd, &buff[i++], 1);
-    if (fd == 2)
-        write(1, "\n", 1);
-}
-
-int init_serv(int port, serv_t *serv)
-{
-  struct sockaddr_in servaddr;
-
-  // socket create and verification
-  serv->sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (serv->sockfd == -1)
-  {
-    ft_message(2, "Fatal error");
-    exit(1);
-  }
-  bzero(&servaddr, sizeof(servaddr));
-
-  // assign IP, PORT
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
-  servaddr.sin_port = htons(port);
-
-  // Binding newly created socket to given IP and verification
-  if ((bind(serv->sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-  {
-    ft_message(2, "Fatal error");
-    exit(1);
-  }
-  if (listen(serv->sockfd, 10) != 0)
-  {
-    ft_message(2, "Fatal error");
-    exit(1);
-  }
-  FD_SET(serv->sockfd, &serv->master);
-  return serv->sockfd;
-}
-
-int accept_cli(int sockfd)
-{
-    socklen_t   len;
-    struct sockaddr_in cli;
-    int     connfd;
-
-    len = sizeof(cli);
-    connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-    if (connfd < 0)
-    {
-        ft_message(2, "Fatal error");
-        exit(1);
-    }
-    return connfd;
-}
+  int   sockfd;
+  fd_set  master;
+  fd_set  rfds;
+} serv_t;
 
 int extract_message(char **buf, char **msg)
 {
@@ -126,81 +68,158 @@ char *str_join(char *buf, char *add)
   return (newbuf);
 }
 
+void  ft_message(int fd, char *buff)
+{
+  int   i = 0;
+  while (buff[i])
+    write(fd, &buff[i++], 1);
+  if (fd == 2)
+    write(1, "\n", 1);
+}
+
+int init_serv(int port, serv_t *serv)
+{
+  struct sockaddr_in servaddr;
+
+  // socket create and verification
+  serv->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (serv->sockfd == -1)
+  {
+    ft_message(2, "Fatal error");
+    exit(1);
+  }
+  bzero(&servaddr, sizeof(servaddr));
+
+  // assign IP, PORT
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
+  servaddr.sin_port = htons(port);
+
+  // Binding newly created socket to given IP and verification
+  if ((bind(serv->sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+  {
+    ft_message(2, "Fatal error");
+    exit(1);
+  }
+  if (listen(serv->sockfd, 1024) != 0)
+  {
+    ft_message(2, "Fatal error");
+    exit(1);
+  }
+  FD_SET(serv->sockfd, &serv->master);
+  return serv->sockfd;
+}
+
+int accept_cli(int sockfd)
+{
+  int   connfd;
+  socklen_t len;
+  struct sockaddr_in  cli;
+  len = sizeof(cli);
+  connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+  if (connfd < 0)
+  {
+    ft_message(2, "Fatal error");
+    exit(1);
+  }
+  return connfd;
+}
+
+void send_to_cli(cli_t *c, int fd, char *buff)
+{
+  int   i = 0;
+  while (i < 1024)
+  {
+    if (c[i].fd > 0 && c[i].fd != fd)
+    {
+      send(c[i].fd, buff, strlen(buff), 0);
+    }
+    i++;
+  }
+}
+
 int main(int ac, char **av)
 {
-    if (ac != 2)
+  if (ac != 2)
+  {
+    ft_message(2, "Wrong number of arguments");
+    exit(1);
+  }
+  serv_t  serv;
+  cli_t   c[1024];
+  int   port = atoi(av[1]);
+  int   sockfd = init_serv(port, &serv);
+  int   max = sockfd;
+  int   i = 0;
+  while (i < 1024)
+    c[i++].fd = 0;
+  int   id = 0;
+  while (1)
+  {
+    serv.rfds = serv.master;
+    if (!select(max + 1, &serv.rfds, 0, 0, 0))
     {
-        ft_message(2, "Wrong number of arguments");
-        exit(1);
+      ft_message(2, "Fatal error");
+      exit(1);
     }
-    cli_t   c[1024];
-    serv_t  serv;
-    int     port = atoi(av[1]);
-    int     sockfd = init_serv(port, &serv);
-    int     i = 0;
-    int     id = 0;
-    while (i < 1024)
-      c[i++].fd = 0;
-    int   max = sockfd;
-    while (1)
+    if (FD_ISSET(sockfd, &serv.rfds))
     {
-      serv.rfds = serv.master;
-      if (!select(max + 1, &serv.rfds, 0, 0, 0))
+      int   fd = accept_cli(sockfd);
+      if (fd >= max)
+        max = fd;
+      i = 0;
+      while (c[i].fd != 0 && i < 1024)
+        i++;
+      c[i].fd = fd;
+      c[i].id = id;
+      char  buff[1024];
+      FD_SET(c[i].fd, &serv.master);
+      sprintf(buff, "server: client %d just arrived\n", id);
+      ft_message(1, buff);
+      send_to_cli(c, c[i].fd, buff);
+      id++;
+    }
+    else
+    {
+      int   j = 0;
+      while (j < 1024)
       {
-        ft_message(2, "Fatal error");
-        exit(1);
-      }
-      if (FD_ISSET(sockfd, &serv.rfds))
-      {
-        int   fd = accept_cli(sockfd);
-        if (fd >= max)
-          max = fd;
-        i = 0;
-        while (i < 1024 && i != 0)
-          i++;
-        c[i].fd = fd;
-        c[i].id = id;
-        FD_SET(fd, &serv.master);
-        char  buff[1024];
-        sprintf(buff, "server: client %d just arrived\n", id++);
-        ft_message(1, buff);
-      }
-      else
-      {
-        int   j = 0;
-        while (j < 1024)
+        if (c[j].fd > 0 && FD_ISSET(c[j].fd, &serv.rfds))
         {
-          if (c[j].fd > 0 && FD_ISSET(c[j].fd, &serv.rfds))
+          char  *msg = NULL;
+          msg = (char*)malloc(10000 * sizeof(char));
+          int r = recv(c[j].fd, msg, 10000, 0);
+          if (r <= 0)
           {
-            char  *buff = (char*)malloc(1025 * sizeof(char));
-            int   r = recv(c[j].fd, buff, 1025, 0);
-            if (r <= 0)
-            {
-              char  msg[1024];
-              sprintf(msg, "server: client %d just left\n", c[j].id);
-              ft_message(1, msg);
-              close(c[j].fd);
-              FD_CLR(c[j].fd, &serv.master);
-              c[j].fd = 0;
-            }
-            else
-            {
-              buff[r] = 0;
-              char  *toto = NULL;
-              while (extract_message(&buff, &toto))
-              {
-                char  *join = NULL;
-                join = str_join(join, toto);
-                char  msg[1024];
-                sprintf(msg, "client %d: %s", c[j].id, join);
-                ft_message(1, msg);
-              }
-              free(buff);
-            }
+            char  buff[1024];
+            sprintf(buff, "server: client %d just left\n", c[j].id);
+            ft_message(1, buff);
+            close(c[j].fd);
+            FD_CLR(c[j].fd, &serv.master);
+            c[j].fd = 0;
+            send_to_cli(c, c[j].fd, buff);
           }
-          j++;
+          else
+          {
+            msg[r] = '\0';
+            char  *join = NULL;
+            join = str_join(join, msg);
+            char  *new = NULL;
+            while (extract_message(&join, &new))
+            {
+              char  tab[1024];
+              sprintf(tab, "client %d: %s", c[j].id, new);
+              ft_message(1, tab);
+              send_to_cli(c, c[j].fd, tab);
+              free(new);
+            }
+            free(join);
+          }
+          free(msg);
         }
+        j++;
       }
     }
-    return 0;
+  }
+  return 0;
 }
